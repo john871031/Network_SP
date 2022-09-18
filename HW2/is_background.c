028ef3b2538e43e94d89579286aa171a2cbbe0ec
John <48525510+john871031@users.noreply.github.com>
2022-09-18 16:39:47 +0800

    Create is_background.c

---
+++ HW2/is_background.c
@@ -0,0 +1,32 @@
+/*
+ * is_background.c :  check for & at end
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "shell.h"
+
+int is_background(char ** myArgv) {
+
+  	if (*myArgv == NULL)
+    	return 0;
+
+  	/* Look for "&" in myArgv, and process it.
+  	 *
+	 *	- Return TRUE if found.
+	 *	- Return FALSE if not found.
+	 *
+	 * Fill in code.
+	 */
+	 int i = 0;
+	 while(myArgv[i] != NULL){
+	 	if(*myArgv[i]=='&'){
+	 		
+	 		myArgv[i] = NULL;
+	 		return TRUE;
+	 	}
+	 	i++;
+	 }
+	 return FALSE;
+}
